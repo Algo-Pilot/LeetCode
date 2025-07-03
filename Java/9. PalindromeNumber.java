57bfa490e7abc0c0a91fe682cbbeb85a5179a0f4
Algo-Pilot <fahimasfaq01@gmail.com>
2025-07-03 23:28:09 +0600

    Update 9. PalindromeNumber.java

--- Java/9. PalindromeNumber.java
+++ Java/9. PalindromeNumber.java
@@ -1,3 +1,4 @@
+//with strings
 class Solution {
     public boolean isPalindrome(int x) {
         boolean isPalindrome = false;
@@ -7,6 +8,25 @@ class Solution {
             temp = temp + s.charAt(s.length() - i);
         }
         if(s.equals(temp)){
+            isPalindrome = true;
+        }
+        return isPalindrome;
+    }
+}
+
+//with integers
+class Solution {
+    public boolean isPalindrome(int x) {
+        boolean isPalindrome = false;
+        int temp = x;
+        int div = 0;
+        int dev =0;
+        for(int i =0; temp > 0;i++){
+            div = temp %10;
+            dev = (dev * 10)+ div;
+            temp/=10;
+        }
+        if (dev == x){
             isPalindrome = true;
         }
         return isPalindrome;
